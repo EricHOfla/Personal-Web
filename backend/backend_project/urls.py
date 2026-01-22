d49d6bad64a7d158fc53e9927d849517e79a3c14
Eric Oflah <ericofla1@gmail.com>
2026-01-22 12:16:17 +0200

    Resolve Media

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -1,13 +1,15 @@
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
-from django.conf import settings # type: ignore
-from django.conf.urls.static import static # type: ignore
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend_app.urls')),
 ]
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
