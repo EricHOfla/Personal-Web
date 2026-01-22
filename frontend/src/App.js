19f1620e71113d18b670d0e5909f0c7037a990a0
Eric Oflah <ericofla1@gmail.com>
2026-01-22 11:55:53 +0200

    Resolve merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { getProfile } from "./services/profileService";
-import Spinner from "./components/Spinner";
-import RoundOne from "./components/roundDesigns/RoundOne";
-import RoundTwo from "./components/roundDesigns/RoundTwo";
-import RoundThree from "./components/roundDesigns/RoundThree";
-import RoundFour from "./components/roundDesigns/RoundFour";
-import RoundFive from "./components/roundDesigns/RoundFive";
 import Home from "./Home";
 import "./App.css";
 
@@ -31,11 +25,7 @@ function App() {
   }, []);
 
   if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Spinner />
-      </div>
-    );
+    return <div>Loading...</div>;
   }
   if (error) return <div>Error: {error}</div>;
 
@@ -43,13 +33,6 @@ function App() {
     <div className="w-full lgl:h-screen font-bodyfont overflow-hidden text-textColor bg-black relative">
       <div className="max-w-screen-2xl h-full mx-auto flex justify-center items-center">
         <Home profile={profile} />
-      </div>
-      <div className="w-full h-full absolute top-0 left-0 z-10">
-        <RoundOne />
-        <RoundTwo />
-        <RoundThree />
-        <RoundFour />
-        <RoundFive />
       </div>
     </div>
   );
